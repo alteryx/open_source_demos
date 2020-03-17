6254819d95ab76581c58910a1cbb98862410b18a
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2020-03-17 17:00:31 -0500

    Compose (#15)

--- utils.py
+++ utils.py
@@ -3,12 +3,12 @@ import pandas as pd
 import numpy as np
 import os
 
-def load_entityset(data_dir):
-    order_products = pd.read_csv(os.path.join(data_dir, "order_products__prior.csv"))
-    orders = pd.read_csv(os.path.join(data_dir, "orders.csv"))
-    departments = pd.read_csv(os.path.join(data_dir, "departments.csv"))
-    products = pd.read_csv(os.path.join(data_dir, "products.csv"))
 
+def load_entityset(data_dir, nrows=None):
+    order_products = pd.read_csv(os.path.join(data_dir, "order_products__prior.csv"), nrows=nrows)
+    orders = pd.read_csv(os.path.join(data_dir, "orders.csv"), nrows=nrows)
+    departments = pd.read_csv(os.path.join(data_dir, "departments.csv"), nrows=nrows)
+    products = pd.read_csv(os.path.join(data_dir, "products.csv"), nrows=nrows)
     order_products = order_products.merge(products).merge(departments)
 
 
@@ -62,38 +62,6 @@ def load_entityset(data_dir):
     es["order_products"]["product_name"].interesting_values = ['Banana', 'Bag of Organic Bananas', 'Organic Baby Spinach', 'Organic Strawberries', 'Organic Hass Avocado', 'Organic Avocado', 'Large Lemon', 'Limes', 'Strawberries', 'Organic Whole Milk']
 
     return es
-
-def make_labels(es, training_window, cutoff_time,
-                product_name, prediction_window):
-
-    prediction_window_end = cutoff_time + prediction_window
-    t_start = cutoff_time - training_window
-
-    orders = es["orders"].df
-    ops = es["order_products"].df
-
-    training_data = ops[(ops["order_time"] <= cutoff_time) & (ops["order_time"] > t_start)]
-    prediction_data = ops[(ops["order_time"] > cutoff_time) & (ops["order_time"] < prediction_window_end)]
-
-    users_in_training = training_data.merge(orders)["user_id"].unique()
-
-    valid_pred_data = prediction_data.merge(orders)
-    valid_pred_data = valid_pred_data[valid_pred_data["user_id"].isin(users_in_training)]
-
-    def bought_product(df):
-        return (df["product_name"] == product_name).any()
-
-    labels = valid_pred_data.groupby("user_id").apply(bought_product).reset_index()
-    labels["cutoff_time"] = cutoff_time
-    #  rename and reorder
-    labels.columns = ["user_id", "label", "time",]
-    labels = labels[["user_id", "time", "label"]]
-
-    return labels
-
-def dask_make_labels(es, **kwargs):
-    label_times = make_labels(es, **kwargs)
-    return label_times, es
 
 
 def calculate_feature_matrix(label_times, features):
