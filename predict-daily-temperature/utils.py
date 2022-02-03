a8cab4f7373ed826f9158dcd0cf97f32ca5603c5
Tamar Grey <tamar.grey@alteryx.com>
2022-02-03 18:25:27 -0500

    Add more figures

--- predict-daily-temperature/utils.py
+++ predict-daily-temperature/utils.py
@@ -1,3 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcdefaults()
+
+
+def plot_model_performances(baseline_evalml_score, random_forest_evalml_score, baseline_score, featuretools_score):
+    # data to plot
+    n_groups = 2
+    evalml_runs = (baseline_evalml_score, random_forest_evalml_score)
+    manual_runs = (baseline_score, featuretools_score)
+
+    # create plot
+    fig, ax = plt.subplots()
+    index = np.arange(n_groups)
+    bar_width = 0.35
+    opacity = 0.8
+
+    plt.bar(index, evalml_runs, bar_width,
+            alpha=opacity,
+            color='#9300ff',
+            label='EvalML Runs')
+
+    plt.bar(index + bar_width, manual_runs, bar_width,
+            alpha=opacity,
+            color='gray',
+            label='Manual Runs')
+
+    plt.xlabel('Models')
+    plt.ylabel('Median Absolute Error')
+    plt.title('Model Performance')
+    plt.xticks(index + bar_width/2,
+               ("Baseline Runs", "Feature Engineering Runs"))
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
 
 def feature_importances(X, reg, feats=5):
     feature_imps = [(imp, X.columns[i])
