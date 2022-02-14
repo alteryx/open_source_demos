e5da805d47b83db80794280b7192e2e56f4029a2
Tamar Grey <tamar.grey@alteryx.com>
2022-02-14 09:36:30 -0500

    Create  duplicate notebook forsimplification and simplify code

--- predict-daily-temperature/utils.py
+++ predict-daily-temperature/utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import featuretools as ft
+import pandas as pd
+import sklearn
 
 plt.rcdefaults()
 
@@ -49,11 +51,42 @@ def feature_importances(X, reg, feats=5):
     return [f[1] for f in feature_imps[:feats]]
 
 
-def set_up_entityset(df, id='es', time_index=None):
-    es = ft.EntitySet(id=id)
+def set_up_entityset(df, id_='es', time_index=None):
+    es = ft.EntitySet(id=id_)
     es.add_dataframe(df,
                      dataframe_name='temperatures',
                      index='id',
                      make_index=True,
                      time_index=time_index)
     return es
+
+def read_data(filepath, time_index, target_col):
+    df = pd.read_csv(filepath)
+    return df[[time_index, target_col]]
+
+
+def get_train_test(df, gap):
+    split_point = int(df.shape[0]*.7)
+
+    # leave gap observations between training and test datasets
+    training_data = df[:split_point]
+    test_data = df[(split_point + gap):]
+
+    return training_data, test_data
+
+
+def add_delayed_feature(df, col_to_delay, delay_length):
+    target_delay_training = df[col_to_delay].shift(delay_length)
+    target_delay_training.name = 'target_delay'
+    
+    return pd.concat([df, target_delay_training], axis=1)
+
+def train_and_fit_random_forest_regressor(X_train, y_train, X_test, y_test):
+    reg = sklearn.ensemble.RandomForestRegressor(n_estimators=100)
+    reg.fit(X_train, y_train)
+
+    # Check the accuracy of our model
+    preds = reg.predict(X_test)
+    score = sklearn.metrics.median_absolute_error(preds, y_test)
+    print('Median Abs Error: {:.2f}'.format(score))
+    return reg, score
